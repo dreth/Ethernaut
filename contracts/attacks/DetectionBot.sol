1d875fba540db88fef45714fa707ae058e09bbcb
dreth <github@dac.ac>
2022-07-05 03:17:39 +0200

    detection bot for forta challenge

---
+++ contracts/attacks/DetectionBot.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "../DoubleEntryPoint.sol";
+
+contract DetectionBot is IDetectionBot {
+    address public cryptoVault;
+    Forta public forta;
+
+    constructor(address _cryptoVault, address _forta) public {
+        cryptoVault = _cryptoVault;
+        forta = Forta(_forta);
+    }
+
+    function handleTransaction(address user, bytes calldata) public override {
+        uint dataValue;
+        uint calldataloadPos = 4;
+
+        while (true) {
+            // obtain the next 32-byte item from the calldata
+            assembly {
+                dataValue := calldataload(calldataloadPos)
+            }
+
+            // check if the address is present in the next 32 byte slot
+            if (dataValue == uint256(uint160(cryptoVault))) {
+                // raise alert if address is present in calldata
+                forta.raiseAlert(user);
+                break;
+
+            } else {
+                // add one to calldataloadpos
+                assembly {
+                    calldataloadPos := add(calldataloadPos, 0x1)
+                }
+            }
+        }
+    }
+}
