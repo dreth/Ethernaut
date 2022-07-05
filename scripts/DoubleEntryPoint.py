b540870f2b60c6c8e365a71a3318b6fa02e34a8f
dreth <github@dac.ac>
2022-07-05 03:17:51 +0200

    script for forta challenge, only missing writeup

--- scripts/DoubleEntryPoint.py
+++ scripts/DoubleEntryPoint.py
@@ -1,6 +1,6 @@
 from scripts.helper.utils import *
 from scripts.helper.account import _from, acc
-from brownie import DoubleEntryPoint
+from brownie import DoubleEntryPoint, DetectionBot, Forta, CryptoVault, history
 
 ##########################################
 # Your job is to implement a detection bot and register it in the Forta contract. The bot's implementation will need to raise correct alerts to prevent potential attacks or bug exploits.
@@ -9,18 +9,37 @@ from brownie import DoubleEntryPoint
 # solve the challenge
 def solve_challenge(locally=False):
     # load challenge
+    # not locally
     if not locally:
         dep = load_challenge(ContractObject=DoubleEntryPoint, instance_key='doubleentrypoint')
     else:
         dep = deploy_locally(ContractObject=DoubleEntryPoint, from_account=_from)
-    
+
     # get cryptovault address
     cryptovault_address = dep.cryptoVault()
 
-    
+    # attack the contract
+    cryptovault = CryptoVault.at(cryptovault_address)
 
-    
+    # deploy and set the bot
+    forta = Forta.at(dep.forta())
+    bot = DetectionBot.deploy(cryptovault_address, forta.address, _from)
+    forta.setDetectionBot(bot.address, _from)
 
+    # get the legacy token contract address
+    legacytoken = dep.delegatedFrom()
+
+    # sweep the tokens
+    try:
+        cryptovault.sweepToken(legacytoken, _from | {'allow_revert':True})
+    except:
+        pass
+
+    # transaction was reverted by the contract
+    revert_msg_assertion = history[-1].revert_msg == 'Alert has been triggered, reverting'
+    print(f"The transaction triggered the forta alert: {revert_msg_assertion}")
+
+    return revert_msg_assertion
 
 def main():
     solve_challenge()
